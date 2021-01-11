c9ff2ab3c5531b98230aeb2bc01d385620a84a18
permadiekapermana <permadiekapermana@gmail.com>
2021-01-11 16:19:30 +0700

    soal 7 convert biner positif

--- evaluasiSoal7belum.java
+++ evaluasiSoal7belum.java
@@ -1,27 +1,32 @@
+import java.util.Scanner;
+ 
 public class evaluasiSoal7belum {
-    static int binaryToDecimal(int n)
-    {
-        int num = n;
-        int dec_value = 0;
- 
-        int base = 1;
- 
-        int temp = num;
-        while (temp > 0) {
-            int last_digit = temp % 10;
-            temp = temp / 10;
- 
-            dec_value += last_digit * base;
- 
-            base = base * 2;
+        public static void main(String args[])
+        {
+            Scanner s = new Scanner(System.in);
+            
+            System.out.println("Masukkan Bilangan Biner :");
+            int n = s.nextInt();
+            System.out.println();
+            
+            int decimal, p;
+
+            decimal = 0;
+            p = 0;
+            
+            while(n!=0)
+            {
+                decimal+=((n%10)*Math.pow(2,p));
+                n=n/10;
+                p++;
+            }
+            
+            if (decimal >= 0 && decimal <=127) {
+                System.out.println("Hasil Konversi Binernya adalah :");
+                System.out.println(decimal);
+            } else {
+
+            }
+
         }
- 
-        return dec_value;
-    }
- 
-    public static void main(String[] args)
-    {
-        int num = 10000010;
-        System.out.println(binaryToDecimal(num));
-    }
 }
